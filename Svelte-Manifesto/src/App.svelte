7cd576ac5c74cc69bf65063f6e89be88dd858442
larago <la.arago@gmail.com>
2021-04-16 18:22:01 +0200

    Fetch data to main.js

--- Svelte-Manifesto/src/App.svelte
+++ Svelte-Manifesto/src/App.svelte
@@ -1,9 +1,10 @@
 <script>
-	export let name;
+	export let dades;
+	console.log(dades.index);
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello Project!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
--- Svelte-Manifesto/src/main.js
+++ Svelte-Manifesto/src/main.js
@@ -1,10 +1,16 @@
 import App from './App.svelte';
+import {csv} from "d3-fetch";
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+
+const dades = csv('groupedData.csv',  {encoding: 'utf8'})
+	.then(data => { console.log(data)
+        const app = new App({
+            target: document.body,
+            props: {
+                dades: data
+                
+            }
+        })
+    });
 
 export default app;
