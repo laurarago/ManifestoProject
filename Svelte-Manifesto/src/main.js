c464fe750ac744c6bd4828dee18b93f85e9bb8f7
larago <la.arago@gmail.com>
2021-04-25 13:49:02 +0200

    no important

--- Svelte-Manifesto/src/main.js
+++ Svelte-Manifesto/src/main.js
@@ -1,7 +1,6 @@
 import App from './App.svelte';
 import {json} from "d3-fetch";
 
-
 const groupedData = json('groupedData.json',  {encoding: 'utf8'})
 	.then(data => { const app = new App({
             target: document.body,
